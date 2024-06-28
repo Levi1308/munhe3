dbb16e588fe63bd1dbdde4ba42dd60851f7d7971
Levi Itzhak Akiva <akivalev@post.bgu.ac.il>
2024-06-28 17:49:12 +0200

    Update Hunter.java

--- src/GameTiles/Unit/Player/Hunter.java
+++ src/GameTiles/Unit/Player/Hunter.java
@@ -9,68 +9,47 @@ import GameTiles.Unit.Unit;
 
 import java.util.List;
 
-public class Hunter extends Player{
-
-    private int range;
-    private int arrows_count;
-    private int ticks_count;
-
-    public Hunter(int x, int y, String name, int health_pool, int attack_points, int defense_points, int range) {
-        super(x, y, name, health_pool, attack_points, defense_points);
-        this.range = range;
-        this.arrows_count = 10;
-        this.ticks_count = 0;
-    }
-    @Override
-    public void castAbility() {
-        if (arrows_count > 0) {
-            arrows_count = arrows_count - 1;
-            List<Enemy> enemies_inRange = enemyList_byRange(range);
-            if (enemies_inRange.size() > 0){
-                Enemy e = enemies_inRange.get(0);
-                double minRange = range(e);
-                for (Enemy enemy: enemies_inRange) {
-                    if(range(enemy) < minRange) {
-                        e = enemy;
-                        minRange = range(enemy);
-                    }
-                }
-                manager.sendMessage(getName() + " shot " + e.getName());
-                int defense = e.random_Defense();
-                manager.sendMessage(getName() + " hit " + e.getName() + " for " + (getAttack_points() - defense) + " ability damage.");
-                e.lose_health(getAttack_points() - defense);
-            }
-        }
-        else {
-            manager.sendMessage(getName() + " tried to shoot, but there were not enough arrows.");
-        }
-
+public class Hunter extends Player {
+    private Integer range;
+    private Integer arrows_count;
+    private Integer ticks_count=0;
+    public Hunter(String name, Integer health_pool, Integer health_amount, Integer attack_points, Integer defense_points
+            , GameTile gameTile,Integer range,Integer arrows_count) {
+        super(name, health_pool, health_amount, attack_points, defense_points, gameTile);
+        this.range=range;
+        this.arrows_count=arrows_count;
     }
 
     @Override
-    public void on_GameTick() {
-        if (ticks_count == 10) {
-            arrows_count = arrows_count + getLevel();
-            ticks_count = 0;
-        }
-        else {
-            ticks_count = ticks_count + 1;
-        }
+    public void levelUp() {
+        super.levelUp();
+        arrows_count=arrows_count+10*getLevel();
+        setAttack_points(getAttack_points()+2*getLevel());
+        setDefense_points(getDefense_points()+getLevel());
     }
+    public void OnGameTick()
+    {
+        if(ticks_count==10)
+        {
+            arrows_count--;
+            ticks_count=0;
+        }
+        else
+            ticks_count++;
+    }
+    @Override
+    public void OnAbilityCast() {
 
+    }
+    public String description() {
+        return super.description()+
+                "range: " + range + "\n" +
+                "arrows_count: " + arrows_count + "\n" +
+                "ticks_count: " + ticks_count+"\n";
+    }
     @Override
     public void interact(Unit unit) {
         unit.interact(this);
     }
 
-    public void levelUp() {
-        super.levelUp();
-        arrows_count = arrows_count + (10 * getLevel());
-        setAttack_points(getAttack_points() + (2 * getLevel()));
-        setDefense_points(getDefense_points() + getLevel());
-    }
-
-    public String description(){
-        return super.description() + " Arrows: " + arrows_count;
-    }
 }
