4300e1b9807ec50982be454b1ed84e9773464800
Levi Itzhak Akiva <akivalev@post.bgu.ac.il>
2024-06-28 17:49:30 +0200

    Update Mage.java

--- src/GameTiles/Unit/Player/Mage.java
+++ src/GameTiles/Unit/Player/Mage.java
@@ -20,59 +20,50 @@ public class Mage extends Player{
     private Integer spell_power;
     private Integer hits_count;
     private Integer ability_range;
+    public  Mage(String name, Integer health_pool, Integer health_amount, Integer attack_points, Integer defense_points
+            , GameTile gameTile,Integer manaPool,Integer mana_cost, Integer hits_count,
+                 Integer ability_range) {
+        super(name, health_pool, health_amount, attack_points, defense_points,gameTile);
+        this.mana_pool=manaPool;
+        this.current_mana=this.mana_pool/4;
+        this.mana_cost=mana_cost;
+        this.hits_count=hits_count;
+        this.ability_range=ability_range;
 
-    public Mage (int x, int y, String name, Integer health_pool, Integer attack_points, Integer defense_points, Integer mana_pool, Integer mana_cost, Integer spell_power, Integer hits_count, Integer ability_range) {
-        super(x, y, name, health_pool, attack_points, defense_points);
-        this.mana_pool = mana_pool;
-        this.current_mana = (mana_pool / 4);
-        this.mana_cost = mana_cost;
-        this.spell_power = spell_power;
-        this.hits_count = hits_count;
-        this.ability_range = ability_range;
-    }
 
-    public void castAbility() {
-        if (current_mana > mana_cost) {
-            current_mana = current_mana - mana_cost;
-            int hits = 0;
-            while (hits < hits_count) {
-                List<Enemy> enemies_inRange = enemyList_byRange(ability_range);
-                if (enemies_inRange.size() != 0) {
-                    Random random = new Random();
-                    Enemy random_enemy = enemies_inRange.get(random.nextInt(enemies_inRange.size()));
-                    int defense = random_enemy.random_Defense();
-                    manager.sendMessage(getName() + " hit " + random_enemy.getName() + " for " + (spell_power - defense) + " ability damage.");
-                    random_enemy.lose_health(spell_power - defense);
-                    hits += 1;
-                }
-                else {
-                    break;
-                }
-            }
-        }
-        else {
-            manager.sendMessage(getName() + " tried to cast Blizzard, but there was not enough mana: " + current_mana + '/' + mana_cost);
-        }
     }
 
     @Override
-    public void on_GameTick() {
-        current_mana = Math.min(mana_pool, current_mana + getLevel());
-    }
-
     public void levelUp() {
         super.levelUp();
-        mana_pool = mana_pool + (25 * getLevel());
-        current_mana = Math.min(current_mana + (mana_pool / 4), mana_pool);
-        spell_power = spell_power + (10 * getLevel());
+        mana_pool=mana_pool+25*getLevel();
+        current_mana=Math.min(current_mana+mana_pool/4,mana_pool);
+        spell_power=spell_power+10*getLevel();
     }
+
+    @Override
+    public void OnAbilityCast() {
+
+    }
+    public void OnGameTick()
+    {
+        current_mana=current_mana-mana_cost;
+    }
+    public String description()
+    {
+        return super.description()+
+                "mana_pool: " + mana_pool + "\n" +
+                "current_mana: " + current_mana + "\n" +
+                "mana_cost: " + mana_cost + "\n" +
+                "spell_power: " + spell_power + "\n" +
+                "hits_count: " + hits_count + "\n" +
+                "ability_range: " + ability_range+"\n";
+    }
+
 
     public void interact(Unit unit) {
         unit.interact(this);
     }
 
-    public String description(){
-        return super.description() + ", Mana: " + current_mana +'/' + mana_pool + ", Spell power: " + spell_power;
-    }
 
 }
