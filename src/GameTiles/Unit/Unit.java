7a4e531f3cfe875d4285d620d88327106af9a079
Levi Itzhak Akiva <akivalev@post.bgu.ac.il>
2024-06-28 17:48:31 +0200

    Update Unit.java

--- src/GameTiles/Unit/Unit.java
+++ src/GameTiles/Unit/Unit.java
@@ -19,11 +19,22 @@ import java.util.stream.Collectors;
 public abstract class Unit extends GameTile implements Visitor{
     private String name;
     private Integer health_pool;
-    private Integer current_health;
+    private Integer health_amount;
     private Integer attack_points;
     private Integer defense_points;
+    private GameTile gameTile;
     protected static ArrayList<Enemy> enemyList = new ArrayList<>();
 
+
+    public Unit(String name, Integer health_pool,Integer health_amount, Integer attack_points, Integer defense_points, GameTile gameTile) {
+        this.name = name;
+        this.health_pool = health_pool;
+        this.health_amount = health_amount;
+        this.attack_points = attack_points;
+        this.defense_points = defense_points;
+        this.gameTile=gameTile;
+        //manager.setEnemies(enemyList);
+    }
     public void setName(String name) {
         this.name = name;
     }
@@ -32,8 +43,8 @@ public abstract class Unit extends GameTile implements Visitor{
         this.health_pool = health_pool;
     }
 
-    public void setCurrent_health(Integer current_health) {
-        this.current_health = current_health;
+    public void setHealth_amount(Integer health_amount) {
+        this.health_amount = health_amount;
     }
 
     public void setAttack_points(Integer attack_points) {
@@ -44,12 +55,20 @@ public abstract class Unit extends GameTile implements Visitor{
         this.defense_points = defense_points;
     }
 
+    public GameTile getGameTile() {
+        return gameTile;
+    }
+
+    public void setGameTile(GameTile gameTile) {
+        this.gameTile = gameTile;
+    }
+
     public Integer getHealth_pool() {
         return health_pool;
     }
 
-    public Integer getCurrent_health() {
-        return current_health;
+    public Integer getHealth_amount() {
+        return health_amount;
     }
 
     public Integer getAttack_points() {
@@ -64,16 +83,6 @@ public abstract class Unit extends GameTile implements Visitor{
         return enemyList;
     }
 
-    public Unit(char tile, int x, int y, String name, Integer health_pool, Integer attack_points, Integer defense_points) {
-        super(tile, x, y);
-        this.name = name;
-        this.health_pool = health_pool;
-        this.current_health = health_pool;
-        this.attack_points = attack_points;
-        this.defense_points = defense_points;
-        manager.setEnemies(enemyList);
-    }
-
     public String getName() {
         return name;
     }
@@ -83,20 +92,25 @@ public abstract class Unit extends GameTile implements Visitor{
     }
 
     public String description() {
-        return getName() + ", Health: " + current_health +'/' + health_pool + ", Attack: " + attack_points + ", Defense: " + defense_points;
+        return "Description:\n"+
+                "Name: "+getName()+"\n"+
+                "Health pool: "+this.getHealth_pool()+"\n"+
+                "Health amount: "+this.getHealth_amount()+"\n"+
+                "Attack points: "+getAttack_points()+"\n"+
+                "Defence points: "+getDefense_points()+"\n";
     }
 
     public int random_Defense(){
         Random random = new Random();
         int defense = random.nextInt(defense_points);
-        manager.sendMessage(getName() + " rolled " + defense + " defense points");
+        //manager.sendMessage(getName() + " rolled " + defense + " defense points");
         return defense;
     }
 
     public int random_Attack(){
         Random random = new Random();
         int attack = random.nextInt(attack_points);
-        manager.sendMessage(getName() + " rolled " + attack + " defense points");
+        //manager.sendMessage(getName() + " rolled " + attack + " defense points");
         return attack;
     }
 
@@ -121,7 +135,7 @@ public abstract class Unit extends GameTile implements Visitor{
     }
 
     public boolean isDead(){
-        return current_health <= 0;
+        return health_amount <= 0;
     }
 
     public List<Enemy> enemyList_byRange(int range){
