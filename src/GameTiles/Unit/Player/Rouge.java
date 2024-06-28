9973640532ef30c529924843336a829ec631e847
Levi Itzhak Akiva <akivalev@post.bgu.ac.il>
2024-06-28 17:49:43 +0200

    Update Rouge.java

--- src/GameTiles/Unit/Player/Rouge.java
+++ src/GameTiles/Unit/Player/Rouge.java
@@ -12,46 +12,38 @@ import java.util.List;
 
 public class Rouge extends Player{
     private Integer cost;
-    private Integer current_energy;
-
-    public Rouge(int x, int y, String name, Integer health_pool, Integer attack_points, Integer defense_points, Integer cost) {
-        super(x, y, name, health_pool, attack_points, defense_points);
-        this.cost = cost;
-        current_energy = 100;
-    }
-
-    public void castAbility() {
-        if (current_energy > cost) {
-            manager.sendMessage(getName() + " used Fan of Knives");
-            current_energy = current_energy - cost;
-            List<Enemy> enemies_inRange = enemyList_byRange(2);
-            for (Enemy e : enemies_inRange){
-                int defense = e.random_Defense();
-                manager.sendMessage(getName() + " attacked " + e.getName() + " for " + (getAttack_points() - defense) + " ability damage.");
-                e.lose_health(getAttack_points() - defense);
-            }
-        }
-        else {
-            manager.sendMessage(getName() + " tried to cast Fan of Knives, but there was not enough energy: " + current_energy + '/' + cost);
-        }
+    private Integer current_energy=100;
+    public Rouge(String name, Integer health_pool, Integer health_amount, Integer attack_points, Integer defense_points
+            , GameTile gameTile,Integer cost ) {
+        super(name, health_pool, health_amount, attack_points, defense_points,gameTile);
+        this.cost=cost;
     }
 
     @Override
-    public void on_GameTick() {
-        current_energy = Math.min(current_energy + 10, 100);
+    public void OnAbilityCast() {
+
     }
 
+    @Override
     public void levelUp() {
         super.levelUp();
-        current_energy = 100;
-        setAttack_points(getAttack_points() + (3 * getLevel()));
+        current_energy=100;
+        setAttack_points(getAttack_points()+3*getLevel());
+    }
+    public void OnGameTick()
+    {
+        current_energy=Math.min(current_energy+10,10100);
+    }
+    public String description()
+    {
+        return super.description()+
+                "cost: " + cost + "\n" +
+                "current_energy: " + current_energy+"\n";
     }
 
     public void interact(Unit unit) {
         unit.interact(this);
     }
 
-    public String description() {
-        return super.description() + ", Energy: " + current_energy + "/100";
-    }
+
 }
